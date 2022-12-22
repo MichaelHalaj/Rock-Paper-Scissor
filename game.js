848f9317964fc743456e653cfa201cc0cb3ab2e3
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-22 17:07:37 -0500

    Finish logical expression

--- game.js
+++ game.js
@@ -8,12 +8,19 @@ function computerChoice(){
 function playRound(playerDecision){
     computerDecision = computerChoice();
 
-    formattedComputerDecision = computerDecision.charAt(0).toUpperCase() + computerDecision.substring(0);
+    formattedComputerDecision = computerDecision.charAt(0).toUpperCase() + computerDecision.substring(1);
+    formattedPlayerDecision = playerDecision.charAt(0).toUpperCase() + playerDecision.substring(1);
     console.log(formattedComputerDecision);
-    if(computerDecision == 'rock' && playerDecision == 'paper'
+    if(computerDecision == 'rock' && playerDecision == 'scissors'
     || computerDecision == 'paper' && playerDecision == 'rock'
     || computerDecision == 'scissors' && playerDecision == 'paper'){
-        message = `You lose! ${computerDecision}`;
+        message = `You lose! ${formattedComputerDecision} beats ${formattedPlayerDecision}.`;
+        console.log(message);
+    }else if(computerDecision == 'rock' && playerDecision == 'paper'
+    || computerDecision == 'paper' && playerDecision == 'scissors'
+    || computerDecision == 'scissors' && playerDecision == 'rock'){
+        message = `You win! ${formattedPlayerDecision} beats ${formattedComputerDecision}`;
+        console.log(message);
     }
 }
 
