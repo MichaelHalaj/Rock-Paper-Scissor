a45e6a57b0967e9edae9acf7f7b8d8cefc96ad08
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-22 17:22:11 -0500

    Add showing message

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 let playerScore = 0;
 let computerScore = 0;
+const buttons = document.querySelectorAll('button');
+
 function computerChoice(){
     choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random()*choices.length)];
@@ -7,24 +9,26 @@ function computerChoice(){
 
 function playRound(playerDecision){
     computerDecision = computerChoice();
-
+    message = '';
     formattedComputerDecision = computerDecision.charAt(0).toUpperCase() + computerDecision.substring(1);
     formattedPlayerDecision = playerDecision.charAt(0).toUpperCase() + playerDecision.substring(1);
-    console.log(formattedComputerDecision);
+
     if(computerDecision == 'rock' && playerDecision == 'scissors'
     || computerDecision == 'paper' && playerDecision == 'rock'
     || computerDecision == 'scissors' && playerDecision == 'paper'){
         message = `You lose! ${formattedComputerDecision} beats ${formattedPlayerDecision}.`;
-        console.log(message);
     }else if(computerDecision == 'rock' && playerDecision == 'paper'
     || computerDecision == 'paper' && playerDecision == 'scissors'
     || computerDecision == 'scissors' && playerDecision == 'rock'){
         message = `You win! ${formattedPlayerDecision} beats ${formattedComputerDecision}`;
-        console.log(message);
+    }else{
+        message = `It's a draw! You both pick ${formattedComputerDecision}.`;
     }
+    document.getElementById("result").innerHTML = message;
+    return;
 }
 
-const buttons = document.querySelectorAll('button');
+
 
 buttons.forEach(btn =>{
     btn.addEventListener('click', function(){
