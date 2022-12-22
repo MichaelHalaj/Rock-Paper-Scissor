9663a9f75e5e5aaac26e01c06d1efc49789709ce
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-22 17:33:17 -0500

    Finish basic game

--- game.js
+++ game.js
@@ -6,23 +6,40 @@ function computerChoice(){
     choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random()*choices.length)];
 }
-
+function disableButtons(){
+    buttons.forEach(btn=>{
+        btn.disabled = true;
+    });
+}
 function playRound(playerDecision){
     computerDecision = computerChoice();
     message = '';
     formattedComputerDecision = computerDecision.charAt(0).toUpperCase() + computerDecision.substring(1);
     formattedPlayerDecision = playerDecision.charAt(0).toUpperCase() + playerDecision.substring(1);
-
+    
     if(computerDecision == 'rock' && playerDecision == 'scissors'
     || computerDecision == 'paper' && playerDecision == 'rock'
     || computerDecision == 'scissors' && playerDecision == 'paper'){
-        message = `You lose! ${formattedComputerDecision} beats ${formattedPlayerDecision}.`;
+        computerScore++;
+        message = `Player Score = ${playerScore}<br> Computer Score = ${computerScore} <br>
+            You lose! ${formattedComputerDecision} beats ${formattedPlayerDecision}.`;
+        if(computerScore == 5){
+            message += '<br> You lost the game!';
+            disableButtons();
+        }
     }else if(computerDecision == 'rock' && playerDecision == 'paper'
     || computerDecision == 'paper' && playerDecision == 'scissors'
     || computerDecision == 'scissors' && playerDecision == 'rock'){
-        message = `You win! ${formattedPlayerDecision} beats ${formattedComputerDecision}`;
+        playerScore++;
+        message = `Player Score = ${playerScore}<br> Computer Score = ${computerScore}<br>
+        You win! ${formattedPlayerDecision} beats ${formattedComputerDecision}.`;
+        if(playerScore == 5){
+            message += '<br>You won the game!';
+            disableButtons();
+        }
     }else{
-        message = `It's a draw! You both pick ${formattedComputerDecision}.`;
+        message = `Player Score = ${playerScore}<br> Computer Score = ${computerScore}<br>
+        It's a draw! You both pick ${formattedComputerDecision}.`;
     }
     document.getElementById("result").innerHTML = message;
     return;
